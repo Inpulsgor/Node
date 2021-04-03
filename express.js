305141d30c04fb7caf8104cb7f44b3de65fa9715
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-04-03 23:40:49 +0300

    express middleware example

--- express.js
+++ express.js
@@ -2,8 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.get("/home", (request, response, next) => {
-	return response.send({ answer: "here is your answer" });
+app.use(express.json());
+
+app.get(
+	"/home",
+	(request, response, next) => {
+		response.set("Set-Cookie", "some=value");
+
+		const error = new Error();
+		error.status = 400;
+
+		next(error);
+	},
+	(request, response, next) => {
+		return response.send({ answer: "here is your answer" });
+	}
+);
+
+app.post("/home", (request, response, next) => {
+	console.log(request.body);
+
+	response.send(request.body);
 });
 
 app.listen(3000, () => {
