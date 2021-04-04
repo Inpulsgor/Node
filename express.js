11dfc87308a3d26337d7e49a6e77803521e3ed87
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-04-04 13:58:38 +0300

    middleware to catch error

--- express.js
+++ express.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.urlencoded());
+app.use(express.static("public"));
+
+/*
+ * GET /home
+ */
 
 app.get(
 	"/home",
@@ -20,12 +25,36 @@ app.get(
 	}
 );
 
+/*
+ * POST /home
+ */
+
 app.post("/home", (request, response, next) => {
 	console.log(request.body);
 
 	response.send(request.body);
 });
 
-app.listen(3000, () => {
-	console.log("Listening port", 3000);
+/*
+ * POST /login
+ */
+
+app.post("/login", (request, response, next) => {
+	console.log(request.body);
+
+	return response.send("Request was successfully handled");
+});
+
+/*
+ * middleware обробки помилок
+ */
+
+app.use((error, request, response, next) => {
+	delete error.stack;
+
+	next(error);
+});
+
+app.listen(3002, () => {
+	console.log("Listening port", 3002);
 });
