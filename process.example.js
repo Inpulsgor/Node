c437bb3f1ff28d836da9f446321e7c9ec52fce53
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-04-07 14:59:43 +0300

    process directory

---
+++ process.example.js
@@ -0,0 +1,7 @@
+console.log(process.argv); //
+console.log("__dirname", __dirname);
+console.log("__filename", __filename);
+console.log(process.cwd()); // показує абсолютний шлях до папки з якої запускається код
+console.log(process.exit(0)); // моментально завершує виконання скрипта / 0 - значить що скрипт завершився успішно
+// далі скрипт не відпрацює
+console.log(process.env); //
