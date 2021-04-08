f753e176235b05070a1c0a15fad39663ec51ce9e
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-04-08 09:06:58 +0300

    updated

--- process.example.js
+++ process.example.js
@@ -1,7 +1,8 @@
-console.log(process.argv); //
-console.log("__dirname", __dirname);
-console.log("__filename", __filename);
-console.log(process.cwd()); // показує абсолютний шлях до папки з якої запускається код
+console.log("process.argv ------------>", process.argv); //
+console.log("require ------------>", require); //
+console.log("__dirname ------------>", __dirname); //
+console.log("__filename ------------>", __filename); //
+console.log("process.cwd() ------------>", process.cwd()); // показує абсолютний шлях до папки з якої запускається код
+console.log("process.env ------------>", process.env); //
 console.log(process.exit(0)); // моментально завершує виконання скрипта / 0 - значить що скрипт завершився успішно
-// далі скрипт не відпрацює
-console.log(process.env); //
+
