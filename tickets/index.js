7eea3a9c13317ab3b28c355ed56849ae9bc319c7
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-04-15 12:30:20 +0300

    weather API / joi added

--- tickets/index.js
+++ tickets/index.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.get("/hello", (req, res, next) => res.send("Hello there"))
+// Global middleware
+app.use(express.json());
+
+// Requests
+app.get(
+  "/weather",
+  (req, res, next) => {
+	
+  },
+  (req, res, next) => {
+    // console.log("body", req.body);
+    console.log("query", req.query);
+
+    res.json({ weather: "send" });
+  }
+);
+
+// Listen port
+app.listen(port, () => {
+	console.log("Started listening on port", port);
+});
