4734428214820da7f8fac410780180c019395fea
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-04-23 07:57:11 +0300

    mongo / REST api

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -1,6 +1,30 @@
 const mongodb = require('mongodb');
 const { MongoClient } = mongodb;
 
-const link = "";
+const DB_NAME = 'test_db';
 
-console.log(MongoClient.connect(link));
+const main = async () => {
+	const client = await MongoClient.connect(MONGO_DB_URL, {
+		// useNewUrlParser: true,
+		useUnifiedTopology: true
+	});
+	console.log('Successfully connected to DB');
+
+	const db = client.db(DB_NAME);
+	const test_collection = db.collection("test_collection");
+
+	// await test_collection.insertMany([
+	// 	{
+	// 		username: "Tesla",
+	// 		email: "tesla@email.com",
+	// 		age: 18,
+	// 		accepted: true
+	// 	}
+	// ]);
+
+	console.log(
+    await test_collection.find({ $or: [{ username: "Mango" }, { username: "Tesla" }] }).toArray()
+  );
+}
+
+main();
