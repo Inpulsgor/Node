9ff7ef7630744ea21a2ede04829b843997787bad
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-04-23 09:51:05 +0300

    get & create users from MongoDB

--- mongoDB/api/server.js
+++ mongoDB/api/server.js
@@ -1,38 +1,80 @@
 const express = require("express");
 const Joi = require("joi");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectID } = require("mongodb");
 require('dotenv').config();
 
-const PORT = process.env.PORT;
+let db, usersCollection;
 
+/*
+ * CREATE
+ */
 
 const validateCreateUser = (req, res, next) => {
-	const schema = Joi.object({
+	const validationSchema = Joi.object({
 		username: Joi.string().required(),
 		email: Joi.string().required(),
 		password: Joi.string().required()
 	});
 
-	const result = schema.valid(req.body);
+	const validationResult = validationSchema.validate(req.body);
 
-	if(result.error) return res.status(400).send(result.error);
+	if(validationResult.error) return res.status(400).send(validationResult.error);
 
 	next();
 };
 
-const CreateUser = (req, res, next) => {};
+const CreateUser = async (req, res, next) => {
+	try {
+		const newUser = await usersCollection.insert(req.body);
+		return res.status(201).json(newUser.ops);
+	} catch(error) {
+		next(error);
+	}
+};
 
-const main = async() => {
+/*
+ * READ
+ */
+
+const getUsers = async (req, res, next) => {
+	try {
+		const users = await usersCollection.find().toArray();
+		return res.status(200).json(users);
+	} catch(error) {
+		next(error);
+	}
+}
+
+const getUserByID = async (req, res, next) => {
+	try {
+		const userID = req.params.id;
+		const user = await usersCollection.findOne({ _id: new ObjectID(userID) });
+
+		return res.status(200).json(user);
+	} catch (error) {
+		next(error);
+	}
+};
+
+/*
+ * INIT
+ */
+
+const main = async () => {
 	const server = express();
-	const client = await MongoClient.connect(process.env.MONGODB_URL);
-	const db = client.db(process.env.DB_NAME);
-	const users = db.collection("users");
+	const client = await MongoClient.connect(process.env.MONGODB_URL, {
+    	useUnifiedTopology: true,
+  	});
 
-	server.post("/users", )
+	db = client.db(process.env.DB_NAME);
+	usersCollection = db.collection("users");
 
-	server.listen(PORT, () =>
-    console.log("Server listening on port", PORT)
-  );
+	server.use(express.json());
+	server.post("/users", validateCreateUser, CreateUser);
+	server.get("/users", getUsers);
+	server.get("/users/:id", getUserByID);
+
+	server.listen(process.env.PORT, () => console.log("Server listening on port", process.env.PORT));
 }
 
 main();
