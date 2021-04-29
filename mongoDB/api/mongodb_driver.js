675f8d8294bc60b6f24eb6b2c6ea45a3ef9e67ff
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-04-29 18:06:56 +0300

    started develop server

---
+++ mongoDB/api/mongodb_driver.js
@@ -0,0 +1,140 @@
+const express = require("express");
+const Joi = require("joi");
+const { MongoClient, ObjectID } = require("mongodb");
+require('dotenv').config();
+
+let db, usersCollection;
+
+/*
+ * CREATE
+ */
+
+const validateCreateUser = (req, res, next) => {
+	const validationSchema = Joi.object({
+		username: Joi.string().required(),
+		email: Joi.string().required(),
+		password: Joi.string().required()
+	});
+
+	const validationResult = validationSchema.validate(req.body);
+
+	if(validationResult.error) return res.status(400).send(validationResult.error);
+
+	next();
+};
+
+const CreateUser = async (req, res, next) => {
+	try {
+		const newUser = await usersCollection.insert(req.body);
+		return res.status(201).json(newUser.ops);
+	} catch(error) {
+		next(error);
+	}
+};
+
+/*
+ * READ
+ */
+
+const getUsers = async (req, res, next) => {
+	try {
+		const users = await usersCollection.find().toArray();
+		return res.status(200).json(users);
+	} catch(error) {
+		next(error);
+	}
+}
+
+const getUserByID = async (req, res, next) => {
+	try {
+		const userID = req.params.id;
+
+		if (!ObjectID.isValid(userID)) return res.status(404).send(); // if ObjectID is not valid - return 404
+
+		const user = await usersCollection.findOne({ _id: new ObjectID(userID) });
+
+		if (!user) return res.status(404).send(); // if user with current ID not found - return 404
+
+		return res.status(200).json(user);
+  } catch (error) {
+		next(error);
+	}
+};
+
+/*
+ * UPDATE
+ */
+
+const validateUpdateUser = (req, res, next) => {
+  const validationSchema = Joi.object({
+    username: Joi.string(),
+    email: Joi.string(),
+    password: Joi.string()
+  });
+
+  const validationResult = validationSchema.validate(req.body);
+
+  if (validationResult.error) return res.status(400).send(validationResult.error);
+
+  next();
+};
+
+const updateUser = async (req, res, next) => {\
+	try {
+		const userID = req.params.id;
+
+		if (!ObjectID.isValid(userID)) return res.status(404).send(); // if ObjectID is not valid - return 404
+
+		const updateResult = await usersCollection.updateOne({ _id: new ObjectID(userID) }, { $set: req.body });
+
+		if (!updateResult.modifiedCount) return res.status(404).send();
+
+		return res.status(204).send();
+	} catch (error) {
+		next(error);
+	}
+};
+
+/*
+ * DELETE
+ */
+
+const deleteUserByID = async (req, res, next) => {
+	try {
+		const userID = req.params.id;
+
+		if (!ObjectID.isValid(userID)) return res.status(404).send(); // if ObjectID is not valid - return 404
+
+		const deleteResult = await usersCollection.deleteOne({ _id: new ObjectID(userID) });
+
+		if (!deleteResult.deletedCount) return res.status(404).send();
+
+		return res.status(204).send();
+	} catch (error) {
+		next(error);
+	}
+}
+
+/*
+ * INIT
+ */
+
+const main = async () => {
+	const server = express();
+	const client = await MongoClient.connect(process.env.MONGODB_URL, {useUnifiedTopology: true});
+
+	db = client.db(process.env.DB_NAME);
+	usersCollection = db.collection("users");
+
+	server.use(express.json());
+
+	server.post("/users", validateCreateUser, CreateUser);
+	server.get("/users", getUsers);
+	server.get("/users/:id", getUserByID);
+	server.put("/users/:id", validateUpdateUser, updateUser);
+	server.delete("/users/:id", deleteUserByID);
+
+	server.listen(process.env.PORT, () => console.log("Server listening on port", process.env.PORT));
+}
+
+main();
--- mongoDB/api/server.js
+++ mongoDB/api/server.js
@@ -1,140 +1,48 @@
 const express = require("express");
-const Joi = require("joi");
-const { MongoClient, ObjectID } = require("mongodb");
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-let db, usersCollection;
-
-/*
- * CREATE
- */
-
-const validateCreateUser = (req, res, next) => {
-	const validationSchema = Joi.object({
-		username: Joi.string().required(),
-		email: Joi.string().required(),
-		password: Joi.string().required()
-	});
-
-	const validationResult = validationSchema.validate(req.body);
-
-	if(validationResult.error) return res.status(400).send(validationResult.error);
-
-	next();
-};
-
-const CreateUser = async (req, res, next) => {
-	try {
-		const newUser = await usersCollection.insert(req.body);
-		return res.status(201).json(newUser.ops);
-	} catch(error) {
-		next(error);
-	}
-};
+const URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT;
 
 /*
- * READ
+ * create server
+ * init global middleware
+ * init routes
+ * init database
+ * start listening
  */
 
-const getUsers = async (req, res, next) => {
-	try {
-		const users = await usersCollection.find().toArray();
-		return res.status(200).json(users);
-	} catch(error) {
-		next(error);
-	}
-}
+module.exports = class UserServer {
+	constructor() {
+		this.server = null;
+	};
 
-const getUserByID = async (req, res, next) => {
-	try {
-		const userID = req.params.id;
+	async start() {
+		this.initServer();
+		this.initMiddleware();
+		this.initRoutes();
+		await this.initDatabase();
+		this.startListening();
+	};
 
-		if (!ObjectID.isValid(userID)) return res.status(404).send(); // if ObjectID is not valid - return 404
+	initServer() {
+		this.server = express();
+	};
 
-		const user = await usersCollection.findOne({ _id: new ObjectID(userID) });
+	initMiddleware() {
+		this.server.use(express.json());
+	};
 
-		if (!user) return res.status(404).send(); // if user with current ID not found - return 404
+	initRoutes() {
 
-		return res.status(200).json(user);
-  } catch (error) {
-		next(error);
-	}
-};
+	};
 
-/*
- * UPDATE
- */
+	async initDatabase() {
+		await mongoose.connect();
+	};
 
-const validateUpdateUser = (req, res, next) => {
-  const validationSchema = Joi.object({
-    username: Joi.string(),
-    email: Joi.string(),
-    password: Joi.string()
-  });
-
-  const validationResult = validationSchema.validate(req.body);
-
-  if (validationResult.error) return res.status(400).send(validationResult.error);
-
-  next();
-};
-
-const updateUser = async (req, res, next) => {
-	try {
-		const userID = req.params.id;
-
-		if (!ObjectID.isValid(userID)) return res.status(404).send(); // if ObjectID is not valid - return 404
-
-		const updateResult = await usersCollection.updateOne({ _id: new ObjectID(userID) }, { $set: req.body });
-
-		if (!updateResult.modifiedCount) return res.status(404).send();
-
-		return res.status(204).send();
-	} catch (error) {
-		next(error);
-	}
-};
-
-/*
- * DELETE
- */
-
-const deleteUserByID = async (req, res, next) => {
-	try {
-		const userID = req.params.id;
-
-		if (!ObjectID.isValid(userID)) return res.status(404).send(); // if ObjectID is not valid - return 404
-
-		const deleteResult = await usersCollection.deleteOne({ _id: new ObjectID(userID) });
-
-		if (!deleteResult.deletedCount) return res.status(404).send();
-
-		return res.status(204).send();
-	} catch (error) {
-		next(error);
-	}
-}
-
-/*
- * INIT
- */
-
-const main = async () => {
-	const server = express();
-	const client = await MongoClient.connect(process.env.MONGODB_URL, {useUnifiedTopology: true});
-
-	db = client.db(process.env.DB_NAME);
-	usersCollection = db.collection("users");
-
-	server.use(express.json());
-
-	server.post("/users", validateCreateUser, CreateUser);
-	server.get("/users", getUsers);
-	server.get("/users/:id", getUserByID);
-	server.put("/users/:id", validateUpdateUser, updateUser);
-	server.delete("/users/:id", deleteUserByID);
-
-	server.listen(process.env.PORT, () => console.log("Server listening on port", process.env.PORT));
-}
-
-main();
+	startListening() {
+		this.server.listen(PORT, () => console.log("Server started listening on port", PORT));
+	};
+ }
